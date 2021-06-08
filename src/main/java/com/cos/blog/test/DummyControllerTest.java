fdb1c4c5aa3491800d9caae7c3ccac89db1d0b43
BEUMGOO <captain_kbg@naver.com>
2021-06-08 23:19:12 +0900

    update delete test

--- src/main/java/com/cos/blog/test/DummyControllerTest.java
+++ src/main/java/com/cos/blog/test/DummyControllerTest.java
@@ -3,14 +3,19 @@ package com.cos.blog.test;
 import java.util.List;
 import java.util.function.Supplier;
 
+import javax.transaction.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cos.blog.model.BlogUser;
@@ -22,6 +27,29 @@ public class DummyControllerTest {
 	
 	@Autowired //DI
 	private BlogUserRepository blogUserRepository;
+
+	@DeleteMapping("/dummy/user/{id}")
+	public String deleteUser(@PathVariable int id) {
+		try {
+			blogUserRepository.deleteById(id);
+		} catch (IllegalArgumentException e) {
+			return "削除失敗しました。";
+		}
+		
+		return "削除成功しました。";
+	}
+	
+	@Transactional //関数の実行が終わる時自動的にCommitされる。
+	@PutMapping("/dummy/user/{id}")
+	public BlogUser updateUser(@PathVariable int id, @RequestBody BlogUser requestUser) {
+		BlogUser user = blogUserRepository.findById(id).orElseThrow(()->{
+			return new IllegalArgumentException("更新に失敗しました。");
+		});
+		user.setEmail(requestUser.getEmail());
+		user.setPassword(requestUser.getPassword());
+		
+		return null;
+	}
 	
 	@GetMapping("/dummy/users")
 	public List<BlogUser> list() {
@@ -30,7 +58,8 @@ public class DummyControllerTest {
 	
 	@GetMapping("/dummy/user")
 	public List<BlogUser> pageList(@PageableDefault(size=1, sort="id", direction=Sort.Direction.DESC) Pageable pageable) {
-		List<BlogUser> users = blogUserRepository.findAll(pageable).getContent();
+		Page<BlogUser> pageUsers = blogUserRepository.findAll(pageable);
+		List<BlogUser> users = pageUsers.getContent();
 		return users;
 	}
 	
