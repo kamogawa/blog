5253b9ce3635a4d38262127f6fe3e6883a174d05
BEUMGOO <captain_kbg@naver.com>
2021-06-08 22:17:58 +0900

    pagination test

--- src/main/java/com/cos/blog/test/DummyControllerTest.java
+++ src/main/java/com/cos/blog/test/DummyControllerTest.java
@@ -1,6 +1,15 @@
 package com.cos.blog.test;
 
+import java.util.List;
+import java.util.function.Supplier;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,6 +22,30 @@ public class DummyControllerTest {
 	
 	@Autowired //DI
 	private BlogUserRepository blogUserRepository;
+	
+	@GetMapping("/dummy/users")
+	public List<BlogUser> list() {
+		return blogUserRepository.findAll();
+	}
+	
+	@GetMapping("/dummy/user")
+	public List<BlogUser> pageList(@PageableDefault(size=1, sort="id", direction=Sort.Direction.DESC) Pageable pageable) {
+		List<BlogUser> users = blogUserRepository.findAll(pageable).getContent();
+		return users;
+	}
+	
+	@GetMapping("/dummy/user/{id}")
+	public BlogUser detail(@PathVariable int id) {
+		BlogUser user = blogUserRepository.findById(id).orElseThrow(new Supplier<IllegalArgumentException>() {
+			@Override
+			public IllegalArgumentException get() {
+				// TODO Auto-generated method stub
+				return new IllegalArgumentException("存在しないユーザーです。");
+			}
+		});
+		return user;
+		
+	}
 	
 	@PostMapping("/dummy/join")
 	public String join(BlogUser user) {
