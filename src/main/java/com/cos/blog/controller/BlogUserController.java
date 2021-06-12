c61630917285e6747c756c9323f64f38615a96af
BEUMGOO <captain_kbg@naver.com>
2021-06-12 13:58:36 +0900

    join/login from

--- src/main/java/com/cos/blog/controller/BlogUserController.java
+++ src/main/java/com/cos/blog/controller/BlogUserController.java
@@ -1,6 +1,20 @@
 package com.cos.blog.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
 public class BlogUserController {
-
+	
+	@GetMapping("/blogUser/joinForm")
+	public String joinForm() {
+		
+		return "blogUser/joinForm";
+	}
+	
+	@GetMapping("/blogUser/loginForm")
+	public String loginForm() {
+		
+		return "blogUser/loginForm";
+	}
 }
