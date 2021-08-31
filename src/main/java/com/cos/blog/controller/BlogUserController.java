1f1e684e0e0ed4998cc59b06f040871bcb38ebf7
BEUMGOO <captain_kbg@naver.com>
2021-08-31 23:35:46 +0900

    kakao ログイントークン取得

--- src/main/java/com/cos/blog/controller/BlogUserController.java
+++ src/main/java/com/cos/blog/controller/BlogUserController.java
@@ -1,7 +1,15 @@
 package com.cos.blog.controller;
 
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.client.RestTemplate;
 
 //　ログインできないユーザーを/auth/**に設定
 // '/'の場合、Index.jspを設定
@@ -22,5 +30,38 @@ public class BlogUserController {
 	@GetMapping("/blogUser/updateForm")
 	public String updateForm() {
 		return "blogUser/updateForm";
+	}
+	
+	@GetMapping("/auth/kakao/callback") 
+	public @ResponseBody String kakaoCallback(String code) {
+
+		//Postでkey=valueデータをリクエスト
+		//retrofit2, okhttp, resttemplate など
+		RestTemplate rt = new RestTemplate();
+		
+		//Header object
+		HttpHeaders header = new HttpHeaders();
+		header.add("Content-type", "application/x-www-form-urlencoded;charset=utf-8");
+		
+		//body object
+		MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+		params.add("grant_type", "authorization_code");
+		params.add("client_id", "b59f83beccbe2fa3b0046b00e13d5fce");
+		params.add("redirect_uri", "http://localhost:8000/auth/kakao/callback");
+		params.add("code", code);
+		
+		//Headerとbodyを設定する
+		HttpEntity<MultiValueMap<String, String>> kakaoTokenRequest =
+			new HttpEntity<>(params, header);
+		
+		//リクエストを行う
+		ResponseEntity<String> response = rt.exchange(
+				"Https://kauth.kakao.com/oauth/token",
+				HttpMethod.POST,
+				kakaoTokenRequest,
+				String.class
+			);
+		
+		return "OK!!"+response;
 	}
 }
--- src/main/webapp/WEB-INF/views/blogUser/loginForm.jsp
+++ src/main/webapp/WEB-INF/views/blogUser/loginForm.jsp
@@ -13,6 +13,7 @@
 			class="form-control" name="password" placeholder="Enter password" id="password">
 		</div>
 		<button id="btn-login" class="btn btn-primary">登録</button>
+		<a href="https://kauth.kakao.com/oauth/authorize?client_id=b59f83beccbe2fa3b0046b00e13d5fce&redirect_uri=http://localhost:8000/auth/kakao/callback&response_type=code"><img height="38px" src="/image/kakao_login_button.png" src=""></a>
 	</form>
 </div>
 <%@ include file="../layout/footer.jsp"%>
